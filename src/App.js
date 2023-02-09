08cc6b70c0b4101fbab1d1434ce5bbc89f8ed845
odsay <odsay@hotmail.com>
2023-02-09 23:07:22 +0900

    Movie App Warming Up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from "react";
 
-class App extends React.Component{
-
-  state = {
-    count: 0,
-  }
-
-  add = () => {
-    this.setState(current => ({count: current.count+1}));
-  }
+class App extends React.Component {
   
-  sub = () => {
-    this.setState(current => ({count: current.count-1}));
+  state = {
+    isLoading: true,
+    movies: [],
+  }
+
+  componentDidMount(){
+    // 영화로딩
+    setTimeout(()=>{
+      this.setState({isLoading: false});
+    }, 3000);
   }
 
   render(){
+    const { isLoading } = this.state;
     return <div>
-      <h1>The Number is {this.state.count}.</h1>
-      <button onClick={this.add}>Add</button>&nbsp;
-      <button onClick={this.sub}>Sub</button>
-    </div>
-    ;
+      {isLoading? 'Loading...' : 'We are ready.'}
+    </div>;
+
   }
 }
 
