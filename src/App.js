d7f4a54f1c8226c3c75bf87770eeec6a6220b966
odsay <odsay@hotmail.com>
2023-02-12 01:32:49 +0900

    Movie App - insert HTML tag & create CSS file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   
@@ -21,24 +22,28 @@ class App extends React.Component {
   render(){
     const {isLoading, movies} = this.state;
     return (
-      <div>
-        { isLoading 
-          ? 'Loading...'
-          : movies.map((movie)=>{
-            console.log(movie);
-            return (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                title={movie.title}
-                year={movie.year}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-              />
-            );
-          })
+      <section className='container'>
+        {isLoading ? ( 
+          <div className='loader'><span className='loader__text'>Loading...</span>
+          </div>
+          ):(
+            <div className='movies'>
+              {movies.map(movie=>{
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  title={movie.title}
+                  year={movie.year}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+              })}
+            </div>
+          )
         }
-      </div>
+      </section>
     );
   }
 
